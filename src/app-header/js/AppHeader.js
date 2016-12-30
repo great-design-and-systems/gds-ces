d1d3aed30c4b89fab68422f9095145690eb206c7
Jerico <rickzx98@gmail.com>
2016-12-31 04:38:55 +0800

    updates

--- src/app-header/js/AppHeader.js
+++ src/app-header/js/AppHeader.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Sticky } from 'react-sticky';
 import { connect } from 'react-redux';
 
 export default class AppHeader extends React.Component {
@@ -6,6 +7,15 @@ export default class AppHeader extends React.Component {
         super();
     }
     render() {
-        return (<div></div>);
+        return (
+            <Sticky class="app-header top-bar">
+                <div class="top-bar-title">
+                    <a href="#/"><strong>LibCat</strong></a>
+                </div>
+                <div>
+                    <div class="top-bar-left"></div>
+                    <div class="top-bar-right"></div>
+                </div>
+            </Sticky>);
     }
 }
--- src/app/js/AppComponent.js
+++ src/app/js/AppComponent.js
@@ -1,6 +1,8 @@
 import { Api } from '../../api/ApiService';
+import AppHeader from '../../app-header/js/AppHeader';
 import AppSplash from '../../app-splash/js/AppSplash';
 import React from 'react';
+import { StickyContainer } from 'react-sticky';
 import { connect } from 'react-redux';
 
 const GDS_API = process.env.GDS_API || 'http://localhost:8080/gds';
@@ -8,6 +10,7 @@ const GDS_API = process.env.GDS_API || 'http://localhost:8080/gds';
 @connect()
 export default class App extends React.Component {
     componentWillMount() {
+        this.setState({});
         new Api().init(GDS_API, err => {
             if (!err) {
                 this.setState({ loaded: true });
@@ -21,6 +24,13 @@ export default class App extends React.Component {
     }
     render() {
         let app = <AppSplash header={'LibCat'} message={'Loading awesomeness...'} />
+        if (this.state.loaded) {
+            app = (
+                <StickyContainer>
+                    <AppHeader />
+                </StickyContainer>
+            );
+        }
         return (
             <div> {app} </div>
         );
