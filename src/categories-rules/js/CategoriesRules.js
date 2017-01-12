125b9fb68b82340a809a404e2ec309356699f906
m27 <m27@topdanmark.dk>
2017-01-12 23:36:34 +0800

    updates

--- src/categories-rules/js/CategoriesRules.js
+++ src/categories-rules/js/CategoriesRules.js
@@ -1,25 +1,60 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
+import Rule from './components/Rule';
 export class CategoriesRules extends React.Component {
+    componentWillMount() {
+        this.setState({ rules: [] });
+    }
+    handleRuleChange() {
+        if (this.props.onChange) {
+            this.props.onChange(this.state.rules);
+        }
+    }
     render() {
         return (<div class="categories-rules row expanded">
-            <div class="column large-6">
-                <h5>In Use</h5>
-            </div>
-            <div class="column large-6">
-                <h5>Rules</h5>
-                <table></table>
-            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Pre Action</th>
+                        <th>Action</th>
+                        <th>View </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <Rule onChange={this.handleRuleChange.bind(this) } name="needsApproval" label="Needs Approval" rules={this.state.rules}/>
+                        <Rule onChange={this.handleRuleChange.bind(this) } name="playVideo" label="Play Video" rules={this.state.rules}/>
+                        <Rule onChange={this.handleRuleChange.bind(this) } name="displayGrid" label="Display as Grid" rules={this.state.rules}/>
+                    </tr>
+                    <tr>
+                        <Rule onChange={this.handleRuleChange.bind(this) } name="needsCredentials" label="Needs Credentials" rules={this.state.rules}/>
+                        <Rule onChange={this.handleRuleChange.bind(this) } name="viewDocument" label="View Document" rules={this.state.rules}/>
+                        <Rule onChange={this.handleRuleChange.bind(this) } name="displayList" label="Display as List" rules={this.state.rules}/>
+                    </tr>
+                    <tr>
+                        <Rule onChange={this.handleRuleChange.bind(this) } name="needsSchedule" label="Needs Schedule" rules={this.state.rules}/>
+                    </tr>
+                </tbody>
+            </table>
         </div>)
     }
 }
 
 export class CategoriesRulesField extends React.Component {
+    componentWillReceiveProps(nextProps) {
+        if (this.updated) {
+            this.updated = false;
+            this.props.formManager.triggerValidateHandler(this.props.field, this.props.dispatch);
+        }
+    }
+    handleOnChange(rules) {
+        this.updated = true;
+        this.props.formManager.setModelValue(this.props.field, rules);
+    }
     render() {
         return (<label class="categories-rule-field">
             {this.props.field.label} {this.props.field.isRequired() ? <span class="error">*</span> : ''}
-            <CategoriesRules />
+            <CategoriesRules onChange={this.handleOnChange.bind(this) } />
         </label>)
     }
 }
---
+++ src/categories-rules/js/components/Rule.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import lodash from 'lodash';
+export default class Rule extends React.Component {
+    componentWillMount() {
+        this.setState({});
+    }
+    handleChange(event) {
+        const isOn = $(event.target).is(':checked');
+        if (this.props.rules) {
+            if (isOn) {
+                if (!contains(this.props.rules, this.props.name)) {
+                    this.props.rules.push(this.props.name);
+                }
+            } else {
+                if (contains(this.props.rules, this.props.name)) {
+                    removeItem(this.props.rules, this.props.name);
+                }
+            }
+        }
+        if (this.props.onChange) {
+            this.props.onChange();
+        }
+    }
+    render() {
+        return (<td class="rule">
+            <div class="row">
+                <div class="switch tiny">
+                    <input onChange={this.handleChange.bind(this) } class="switch-input" id={this.props.name} type="checkbox" name={this.props.name} />
+                    <label class="switch-paddle" for={this.props.name}>
+                        <span class="switch-active" aria-hidden="true">Yes</span>
+                        <span class="switch-inactive" aria-hidden="true">No</span>
+                    </label>
+                </div>
+                <div class="column"></div>
+                <label for={this.props.name}>{this.props.label}</label>
+            </div>
+        </td>);
+    }
+}
+
+function contains(list, item) {
+    return lodash.findIndex(list, i => i === item) > -1;
+}
+
+function removeItem(list, item) {
+    const index = lodash.findIndex(list, i => i === item);
+    list.splice(index, 1);
+}
