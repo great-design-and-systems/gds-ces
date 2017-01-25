36cdbca76f34a3bf5fb0706837d9b565d6b3bfcd
Jerico <rickzx98@gmail.com>
2017-01-25 10:16:27 +0800

    updates

--- src/common-fileupload/js/CommonFileupload.js
+++ src/common-fileupload/js/CommonFileupload.js
@@ -19,24 +19,35 @@ export default class CommonFileUpload extends React.Component {
             fileFieldName: 'file',
             chooseFile: (files) => {
                 this.setState({ files });
+            },
+            uploading: (progress) => {
+                this.setState({
+                    loaded: progress.loaded,
+                    total: progress.total
+                });
             }
         }
     }
     handleClearUpload() {
         this.setState({
-            files: []
+            files: [],
+            loaded: 0,
+            total: 0
         });
     }
     render() {
         const hasFile = this.state.files && this.state.files.length > 0;
         const controlButton = hasFile ? <button class="button" ref="uploadBtn">upload</button> : <button class="button" ref="chooseBtn">browse</button>;
         const uploadLabel = hasFile ? <a onClick={this.handleClearUpload.bind(this)}><i class="fa fa-eraser" /></a> : <i className={this.props.labelIcon || 'fa fa-upload'} />;
-        return (<div class="common-fileupload input-group">
-            <span class="input-group-label">{uploadLabel}</span>
-            <input value={hasFile ? this.state.files[0].name : ''} readOnly placeholder="browse file" class="input-group-field" type="text" />
-            <FileUpload refs={this.props.name} className={'input-group-button'} options={this.fileOptions}>
-                {controlButton}
-            </FileUpload>
+        return (<div class="common-fileupload">
+            <div class="input-group">
+                <span class="input-group-label">{uploadLabel}</span>
+                <input value={hasFile ? this.state.files[0].name : ''} readOnly placeholder="browse file" class="input-group-field" type="text" />
+                <FileUpload refs={this.props.name} className={'input-group-button'} options={this.fileOptions}>
+                    {controlButton}
+                </FileUpload>
+            </div>
+            <CommonProgressbar limit={this.state.total} value={this.state.loaded} />
         </div>)
     }
 }
